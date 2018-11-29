486cb1ec19a8e7454f800b734c6800dc5de3d086
Thanh Hai <thanhhai9x98@gmail.com>
2018-11-29 23:35:32 +0700

    add class GiaoVien

---
+++ DoAnPTUDCSDL1/PROJECT/Object/GIAOVIEN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT.Object
+{
+    public class GIAOVIEN
+    {
+        public string MaGVien { get; set; }
+        public string TenGVien { get; set; }
+        public bool Phai { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string DiaChi { get; set; }
+        public string MaChuyenDe { get; set; }
+
+        public GIAOVIEN(string maGVien, string tenGVien, bool phai, DateTime ngaySinh, string diaChi, string maChuyenDe)
+        {
+            MaGVien = maGVien;
+            TenGVien = tenGVien;
+            Phai = phai;
+            NgaySinh = ngaySinh;
+            DiaChi = diaChi;
+            MaChuyenDe = maChuyenDe;
+        }
+    }
+
+}
